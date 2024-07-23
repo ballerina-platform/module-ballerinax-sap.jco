dff61d60b07cff334ba81d2df6e3a0c8d7802e1e
RDPerera <r.dilanperera@gmail.com>
2024-07-23 09:49:25 +0530

    Fix error binding issue

--- native/src/main/java/io/ballerina/lib/sap/SAPErrorCreator.java
+++ native/src/main/java/io/ballerina/lib/sap/SAPErrorCreator.java
@@ -17,13 +17,12 @@
  */
 
 package io.ballerina.lib.sap;
+
 import com.sap.conn.idoc.IDocException;
 import com.sap.conn.jco.JCoException;
 import io.ballerina.runtime.api.creators.ErrorCreator;
 import io.ballerina.runtime.api.utils.StringUtils;
 import io.ballerina.runtime.api.values.BError;
-
-import static io.ballerina.lib.sap.ModuleUtils.getModule;
 public class SAPErrorCreator {
     public static BError fromJCoException(JCoException e) {
         return fromJavaException("JCo Error: " + e.getMessage(), e);
@@ -35,12 +34,13 @@ public class SAPErrorCreator {
         return fromBError(message, ErrorCreator.createError(cause));
     }
     public static BError fromBError(String message, BError cause) {
-        return ErrorCreator.createDistinctError("JCo Error: ", getModule(), StringUtils.fromString(message), cause);
+        return ErrorCreator.createError(
+                ModuleUtils.getModule(), "ERROR", StringUtils.fromString(message), cause, null);
     }
 
     public static BError createError(String message, Throwable throwable) {
         BError cause = ErrorCreator.createError(throwable);
         return ErrorCreator.createError(
-                ModuleUtils.getModule(), "SAP ERROR: ", StringUtils.fromString(message), cause, null);
+                ModuleUtils.getModule(), "ERROR", StringUtils.fromString(message), cause, null);
     }
 }
